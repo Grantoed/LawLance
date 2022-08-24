6556e88e094adf19162fa1f1484f49cfe2999009
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2022-08-24 21:04:50 +0300

    Mobile menu added

---
+++ src/js/menu.js
@@ -0,0 +1,14 @@
+export default (() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('nav__btn-menu--is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('mobile-menu--is-open');
+  });
+})();

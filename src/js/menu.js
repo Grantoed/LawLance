da97611df1839b468871a990fc02a293780da4a1
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2022-08-25 00:37:31 +0300

    mobile finished

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,48 @@
 export default (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
+  const refs = {
+    menuBtnRef: document.querySelector('[data-menu-button]'),
+    mobileMenuRef: document.querySelector('[data-menu]'),
+    containerRef: document.querySelector('.header__container'),
+  };
 
-  menuBtnRef.addEventListener('click', () => {
-    const expanded =
-      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+  const menuClassList = [
+    'mobile-menu mobile-menu--is-open',
+    'mobile-menu__nav',
+    'mobile-menu__list',
+    'mobile-menu__item',
+    'mobile-menu__link',
+    'nav__btn-menu',
+  ];
 
-    menuBtnRef.classList.toggle('nav__btn-menu--is-open');
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
+  refs.menuBtnRef.addEventListener('click', onMenuClick);
 
-    mobileMenuRef.classList.toggle('mobile-menu--is-open');
-  });
+  function onMenuClick() {
+    const menuIsOpen = refs.containerRef.classList.contains('menu-open');
+
+    if (!menuIsOpen) {
+      openMenu();
+    } else {
+      closeMenu();
+    }
+  }
+
+  function openMenu() {
+    refs.containerRef.classList.add('menu-open');
+
+    document.body.classList.add('menu-open');
+    refs.menuBtnRef.classList.add('nav__btn-menu--is-open');
+    refs.menuBtnRef.setAttribute('aria-expanded', true);
+
+    refs.mobileMenuRef.classList.add('mobile-menu--is-open');
+  }
+
+  function closeMenu() {
+    setTimeout(() => refs.containerRef.classList.remove('menu-open'), 250);
+    document.body.classList.remove('menu-open');
+
+    refs.menuBtnRef.classList.remove('nav__btn-menu--is-open');
+    refs.menuBtnRef.setAttribute('aria-expanded', false);
+
+    refs.mobileMenuRef.classList.remove('mobile-menu--is-open');
+  }
 })();

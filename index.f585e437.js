a470d5c65a594dae264ad81c73bfe137eb0b7c6f
Grantoed <ruslan.nadirovich@gmail.com>
2022-08-24 21:37:57 +0000

    Deploying to gh-pages from @ Grantoed/LawLance@da97611df1839b468871a990fc02a293780da4a1 🚀

--- index.f585e437.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t;e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("nav__btn-menu--is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("mobile-menu--is-open")}))}();
-//# sourceMappingURL=index.f585e437.js.map
